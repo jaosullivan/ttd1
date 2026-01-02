3015bced3a25f5d784bbb8d272ca7661011676bd
jaosullivan <johnaosullivan@hotmail.com>
2026-01-02 13:18:16 +0800

    Front Page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,23 +13,39 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do' , self.browser.title)
-        self.fail('Finish the test!')    
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text  # (1)
+        self.assertIn("To-Do", header_text)
 
-        # She is invited to enter a to-do item staright away
-        # She types "Buy peacock feathers" into a text box (Edith's hobby)
-        # is tying fly-fishing lures)
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, "id_new_item")  # (1)
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        
+        # She types "Buy peacock feathers" into a text box 
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")  # (2)
 
-        # When she hots enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)  # (3)
+        time.sleep(1)  # (4)
+        
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")  # (1)
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows),
+        "New to-do item did not appear in table"
+        )
+        
         
         # There is still a text box inviting her to add another item. She 
-        # enters "Use peacock feathers to make fly" 
+        # enters "Use peacock feathers to make fly" (Edith is very
+        # methodical
+        self.fail('Finish the test!')
         
         # The page updates again, and now shows both items on her list
         
